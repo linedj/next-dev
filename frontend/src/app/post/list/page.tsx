a62a70efe209dc67f56c3ad844076fda3b6f6bc4
linedj <eunjun998@gmail.com>
2025-02-13 12:18:14 +0900

    post list rendering

--- frontend/src/app/post/list/page.tsx
+++ frontend/src/app/post/list/page.tsx
@@ -6,22 +6,35 @@ export default async function Page() {
   }
 
   const rsData = await response.json();
-
-  console.log(rsData.code);
-  console.log(rsData.msg);
-  console.log(rsData.data);
+  const pageDto = rsData.data;
 
   return (
     <div>
       <h1>글 목록</h1>
+
       <div>응답 코드 : {rsData.code}</div>
-      <div>결과 메세지 : {rsData.msg}</div>
+      <div>결과 메시지 : {rsData.msg}</div>
+
+      <div>totalPages : {pageDto.totalPages}</div>
+      <div>totalItems : {pageDto.totalItems}</div>
+      <div>currentPageNo : {pageDto.currentPageNo}</div>
+      <div>pageSize : {pageDto.pageSize}</div>
 
       <hr />
 
       <ul>
-        <li>글1</li>
-        <li>글2</li>
+        {pageDto.items.map((item: any) => {
+          return (
+            <li className="border-2 border-red-500 my-2 p-2" key={item.id}>
+              <div>id : {item.id}</div>
+              <div>title : {item.title}</div>
+              <div>authorId : {item.authorId}</div>
+              <div>authorName : {item.authorName}</div>
+              <div>published : {item.published}</div>
+              <div>listed : {item.listed}</div>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
